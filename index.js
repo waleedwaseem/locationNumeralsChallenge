df892317a2b8b5d294ac2663f7eb725340c3ae8a
Waleed Waseem <waleed.waseem@careaxiom.com>
2019-03-12 02:21:57 +0500

    Added last but not least, the conversion of an existing ones to the abbreviated form.

--- index.js
+++ index.js
@@ -61,6 +61,46 @@ app.post('/convertNumerals', function(req, res) {
 	res.json(total);    
 });
 
+app.post('/abbForm', function(req, res) {
+	let numerals = req.body.value;
+	let base = 2;
+	let stack = [];
+
+	for (var i=0; i<numerals.length; i++) {
+		for (var key in dictionary) {
+			if (dictionary[key] == numerals[i])
+				stack.push(Math.pow(base, key))
+		}
+	}
+
+	var total = 0;
+	for (var j in stack) {
+		total += stack[j]
+	}
+
+	let number = total;
+	i = 0;
+	stack = [];
+	
+	if (typeof parseInt(number) !== 'number' || !number) {
+		res.json('Value is not an integer');
+	}
+
+	while (number > 0) {
+		while (Math.pow(base, i) <= number) {
+		  i++;
+		}
+		i--;
+		stack.push(dictionary[i])
+		number = number - Math.pow(base, i)
+		i = 0;	
+	}
+
+	stack.reverse();
+	res.json(stack);    
+
+});
+
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
